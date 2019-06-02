9dd18145cb51fbc16da618f977a2e0f653cfb164
blueskan <batikan17@gmail.com>
2019-06-02 19:20:28 +0300

    Batikan demonstrates simple usage of cryptography functions.

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
 import "fmt"
+import "github.com/blueskan/harpocrates/core"
 
 func main() {
-	// Committer setups
-	fmt.Println("Hello")
+	cryptoManager := core.NewCryptoManager(4096)
+
+	pri, pub := cryptoManager.CreatePubPriKey()
+
+	fmt.Println("PRIVATE KEY:")
+	fmt.Println(string(cryptoManager.PrivateKeyToBytes(pri)))
+
+	fmt.Println("PUBLIC KEY:")
+	fmt.Println(string(cryptoManager.PublicKeyToBytes(pub)))
+
+	fmt.Println("ENCRYPT WITH PUB KEY")
+
+	encryptedString := cryptoManager.EncryptWithPublicKey([]byte("hello world!"), pub)
+
+	fmt.Println(string(encryptedString))
+
+	fmt.Println("DECRYPT WITH PRI KEY")
+	fmt.Println(string(cryptoManager.DecryptWithPrivateKey(encryptedString, pri)))
 }
